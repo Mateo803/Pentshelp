4060b9cd261f036168dd66b84eabde245132159c
Mateo803 <55462135+Mateo803@users.noreply.github.com>
2022-06-08 22:44:28 +0200

    Update pentshelp.py

--- pentshelp.py
+++ pentshelp.py
@@ -7,13 +7,13 @@ try:
     import src.exploitation
     import src.postexploitation
     import src.reporting
-	
-	
+    
+    
 except:
 
-	print('\nThere are dependencies not installed. Please execute install_dependencies.sh first')
-	
-	exit()
+    print('\nThere are dependencies not installed. Please execute install_dependencies.sh first')
+    
+    exit()
 
 
 if not path.exists('pentesting_files'):
@@ -21,7 +21,7 @@ if not path.exists('pentesting_files'):
     print('\nYou need to create the pentshelp folders first')
 
     exit()
-	
+    
 
 def main():
 
@@ -29,43 +29,41 @@ def main():
 
     #Enumeration options
 
-    parser.add_argument("-p","--ports", help="Shows open ports on the selected target")
+    parser.add_argument("-p","--ports", help="Shows open ports on the selected target",metavar=('TARGET'))
 
-    parser.add_argument("--os", help="Shows the operating system of the selected target")
+    parser.add_argument("--os", help="Shows the operating system of the selected target",metavar=('TARGET'))
 
-    parser.add_argument("--directories", help="Shows the directories of the select target (web server)", nargs=2)
+    parser.add_argument("--directories", help="Shows the directories of the select target (web server)", nargs=2,metavar=('TARGET','WORDLIST'))
 
-    parser.add_argument("--subdomains", help="Shows the subdomains of the select target (web server)", nargs=2)
+    parser.add_argument("--subdomains", help="Shows the subdomains of the select target (web server)", nargs=2,metavar=('TARGET','WORDLIST'))
 
-    parser.add_argument("-a","--add_host",help="Adds hostname to /etc/hosts", nargs=2)
-
-
-    #Vulnerabilities detection options
-
-    parser.add_argument("--vulnerabilities",help="Detect possible vulnerabilities on the selected target")
+    parser.add_argument("-a","--add_host",help="Adds hostname to /etc/hosts", nargs=2,metavar=('TARGET_HOSTNAME','TARGET_IP'))
 
 
+    #Vulnerabilities detection option
+
+    parser.add_argument("--vulnerabilities",help="Detect possible vulnerabilities on the selected target",metavar=('TARGET'))
 
 
-    #Exploitation options
+    #Exploitation option
 
-    parser.add_argument("-u","--upload_exploit",help="Uploads an exploit to the target machine", nargs=2)
+    parser.add_argument("-u","--upload_exploit",help="Uploads an exploit to the target machine", nargs=3,metavar=('REPOSITORY','USER_TARGET','TARGET'))
 
 
     #Postexploitation option
 
-    parser.add_argument("--pe",help="Detects vulnerabilities related to privilege scalation")
+    parser.add_argument("--pe",help="Detects vulnerabilities related to privilege scalation",metavar=('TARGET'))
 
 
     #Reporting options
 
-    parser.add_argument("-r","--report",help="Reports a vulnerability (CVE) with an optional solution read from a text file (argument -s or --solution)")
+    parser.add_argument("-r","--report",help="Reports a vulnerability (CVE) with an optional solution read from a text file (argument -s or --solution)",metavar=('CVE-ID'))
 
-    parser.add_argument("-s","--solution", help="Text file with a solution to a specific vulnerability ")
+    parser.add_argument("-s","--solution", help="Text file with a solution to a specific vulnerability")
 
-    parser.add_argument("-d","--delete_cve",help="Removes a vulnerability from the database")
+    parser.add_argument("-d","--delete_cve",help="Removes a vulnerability from the database",metavar=('CVE-ID'))
 
-    parser.add_argument("-g","--generate_report",help="Generates the report with all the vulnerabilities previosly registred")
+    parser.add_argument("-g","--generate_report",help="Generates the report with all the vulnerabilities previosly registred",metavar=('REPORT_NAME'))
 
     #Another options
 
@@ -107,7 +105,7 @@ def main():
 
     elif args.upload_exploit is not None:
 
-        src.exploitation.upload_exploit(args.upload_exploit[0], args.upload_exploit[1])  
+        src.exploitation.upload_exploit(args.upload_exploit[0], args.upload_exploit[1], args.upload_exploit[2])  
 
 
     elif args.pe is not None:
